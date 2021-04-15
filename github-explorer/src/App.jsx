1743ef79bd5fe1c9452262711eb6189c602d35b0
jrderjr <junioraguia2323@gmail.com>
2021-04-15 10:50:31 -0300

    chore: estudos de componetização e propriedades do React

--- github-explorer/src/App.jsx
+++ github-explorer/src/App.jsx
@@ -1,5 +1,6 @@
+import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App() {
-    return <h1> Hello Junior e Adria</h1>
+    return <RepositoryList />
 }
